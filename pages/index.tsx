942bda78e11a8c7605017afebaed5807bf4bedac
Vellidin <aloha.velli@gmail.com>
2025-04-18 17:33:49 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>👋 ¡Hola!</h1>
+      <p>Encuestly está en construcción.</p>
+    </div>
+  );
+}
